aae20c47b23caae84e0117407a3e5baea04d1720
Kal-Aster <elitexxl@hotmail.it>
2024-11-14 16:59:28 +0100

    use parsedRiotDocuments to check if document was already parsed

--- src/server/touchRiotDocument.ts
+++ src/server/touchRiotDocument.ts
@@ -1,19 +1,14 @@
 import { TextDocument } from "vscode-languageserver-textdocument";
 
 import getDocumentFilePath from "./getDocumentFilePath";
+import parsedRiotDocuments from "./parsedRiotDocuments";
 import updateRiotDocument from "./updateRiotDocument";
-
-import { getState } from "./state";
 
 export default function touchRiotDocument(
     document: TextDocument
 ) {
-    const {
-        tsLanguageService
-    } = getState();
-
     const filePath = getDocumentFilePath(document);
-    if (tsLanguageService.hasDocument(filePath)) {
+    if (parsedRiotDocuments.has(filePath)) {
         return filePath;
     }
 
